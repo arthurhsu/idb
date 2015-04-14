0384f03d212729e4a773e5b4dbb418ef1a3e2ff8
Arthur Hsu <arthurhsu@chromium.org>
2015-04-14 13:40:41 -0700

    Rollback to polymer 0.5

---
+++ src/mock_data.js
@@ -0,0 +1,20 @@
+(function() {
+  var owners = ['ldap1', 'ldap2', 'ldap3'];
+  var tasks = [];
+  for (var i = 0; i < 10; ++i) {
+    tasks.push({
+      id: 'T' + i,
+      desc: 'Perform task ' + i,
+      owner: owners[i % owners.length],
+      eta: new Date(new Date().getTime() + i * 86400000),
+      pri: i % 5,
+      done: false
+    });
+  }
+
+  var panelNs = document.getElementById('ns');
+  var panelWorking = document.getElementById('working');
+
+  panelNs.tasks = tasks.slice(0, 4);
+  panelWorking.tasks = tasks.slice(5);
+})();
