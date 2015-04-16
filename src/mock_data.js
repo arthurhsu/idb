044f9361fb6a4bf23ae41891552a9b8778fc7565
arthurhsu <artchsu@gmail.com>
2015-04-15 22:04:17 -0700

    fix

--- src/mock_data.js
+++ src/mock_data.js
@@ -18,5 +18,21 @@
 
   panelNs.tasks = tasks.slice(0, 4);
   panelWorking.tasks = tasks.slice(5);
+
+  var uiModel = {};
   filter.owners = owners;
+  filter.priorities = ['P0', 'P1', 'P2', 'P3'];
+  filter.uiModel = uiModel;
+
+  Object.observe(uiModel, function(changes) {
+    var ownerCond = owners;
+    if (uiModel.owner && uiModel.owner != 'All') {
+      ownerCond = [uiModel.owner];
+    }
+    var priCond = filter.priorities;
+    if (uiModel.priority && uiModel.priority != 'All') {
+      priCond = [uiModel.priority];
+    }
+    console.log(uiModel, ownerCond, priCond);
+  });
 })();
